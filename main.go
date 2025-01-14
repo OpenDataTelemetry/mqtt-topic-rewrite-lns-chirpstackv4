ba59fd27e804fcb0ac928c61de031e2c82e776a8
rogeriocassares <roger10cassares@gmail.com>
2025-01-14 10:22:53 -0300

    rename 3LevelSoilDepthMoisture to SoilMoisture3DepthLevels

--- main.go
+++ main.go
@@ -47,7 +47,7 @@ func main() {
 		"application/4ae0c733-e9b5-482f-8542-3d08f8e6d077/device/+/event/up": byte(mqttSubQos), // SmartLight
 		"application/25e85005-adc9-48d6-89e1-b4f677cf18ef/device/+/event/up": byte(mqttSubQos), // WaterTankLevel
 		"application/a7d603f2-3de4-4516-82f5-3323a3a80467/device/+/event/up": byte(mqttSubQos), // WeatherStation
-		"application/e2cbf2fb-fb26-4608-aacc-66115c0521c0/device/+/event/up": byte(mqttSubQos), // 3LevelSoilDepthMoisture
+		"application/e2cbf2fb-fb26-4608-aacc-66115c0521c0/device/+/event/up": byte(mqttSubQos), // SoilMoisture3DepthLevels
 		"application/5239fc35-6b28-4908-89fa-4efa9bf0636e/device/+/event/up": byte(mqttSubQos), // Sprinkler
 	}
 
@@ -123,7 +123,7 @@ func main() {
 			measurement = "WeatherStation"
 
 		case "e2cbf2fb-fb26-4608-aacc-66115c0521c0":
-			measurement = "3LevelSoilDepthMoisture"
+			measurement = "SoilMoisture3DepthLevels"
 
 		case "5239fc35-6b28-4908-89fa-4efa9bf0636e":
 			measurement = "Sprinkler"
